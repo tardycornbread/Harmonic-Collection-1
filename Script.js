d56f4d88f80ec08e0a3e10f7f1e914f41f21bd98
tardycornbread <azykh21@protonmail.com>
2023-12-05 18:18:59 -0500

    12.5-4

--- Script.js
+++ Script.js
@@ -1,24 +1,37 @@
 $(document).ready(function() {
-    $('.text-container').on('click', function() {
+  // Text container click animation
+  $('.text-container').on('click', function() {
       $(this).addClass('text-container-animate');
-    });
+      // Optional: Remove the class after animation ends to reset the animation
+      setTimeout(() => $(this).removeClass('text-container-animate'), 2000);
   });
 
-
+  // 3D movement on image container hover
   $('.image-container').mousemove(function(e) {
-    var amountMovedX = (e.pageX * -1 / 20);
-    var amountMovedY = (e.pageY * -1 / 20);
-    $(this).children('img').css('transform', 'rotateY(' + amountMovedX + 'deg) rotateX(' + amountMovedY + 'deg)');
+      var amountMovedX = (e.pageX * -1 / 20);
+      var amountMovedY = (e.pageY * -1 / 20);
+      $(this).children('img').css('transform', `rotateY(${amountMovedX}deg) rotateX(${amountMovedY}deg)`);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var inceptionContainer = document.getElementById('inceptionContainer');
-    var images = inceptionContainer.getElementsByClassName('Dream');
-    var foldAngle = 20; // Adjust this value as needed
+  // Inception Container click folding effect
+  var inceptionContainer = $('#inceptionContainer');
+  var images = inceptionContainer.find('.Dream');
+  var foldAngle = 20;
 
-    inceptionContainer.addEventListener('click', function() {
-        for (let i = 0; i < images.length; i++) {
-            images[i].style.transform = `rotateX(${i * foldAngle}deg)`;
-        }
-    });
+  inceptionContainer.click(function() {
+      images.each(function(i) {
+          $(this).css('transform', `rotateX(${i * foldAngle}deg)`);
+      });
+  });
+
+  // Racing text effect on clicking a Dream element
+  $('.Dream').click(function() {
+      var textContainer = $('.text-container');
+      textContainer.addClass('racing-text');
+
+      // Optional: Remove the class after animation ends to reset the animation
+      setTimeout(function() {
+          textContainer.removeClass('racing-text');
+      }, 2000);
+  });
 });
