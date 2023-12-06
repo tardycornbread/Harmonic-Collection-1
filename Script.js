d6365117cd6b880df5e2d8a9cac5a0932e761369
tardycornbread <azykh21@protonmail.com>
2023-12-05 19:12:21 -0500

    12.5-5

--- Script.js
+++ Script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-  // Text container click animation
+  
   $('.text-container').on('click', function() {
       $(this).addClass('text-container-animate');
-      // Optional: Remove the class after animation ends to reset the animation
+      
       setTimeout(() => $(this).removeClass('text-container-animate'), 2000);
   });
 
-  // 3D movement on image container hover
+
   $('.image-container').mousemove(function(e) {
       var amountMovedX = (e.pageX * -1 / 20);
       var amountMovedY = (e.pageY * -1 / 20);
       $(this).children('img').css('transform', `rotateY(${amountMovedX}deg) rotateX(${amountMovedY}deg)`);
   });
 
-  // Inception Container click folding effect
+  
   var inceptionContainer = $('#inceptionContainer');
   var images = inceptionContainer.find('.Dream');
   var foldAngle = 20;
@@ -24,12 +24,11 @@ $(document).ready(function() {
       });
   });
 
-  // Racing text effect on clicking a Dream element
   $('.Dream').click(function() {
       var textContainer = $('.text-container');
       textContainer.addClass('racing-text');
 
-      // Optional: Remove the class after animation ends to reset the animation
+     
       setTimeout(function() {
           textContainer.removeClass('racing-text');
       }, 2000);
